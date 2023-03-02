ddc77a0e985bc50cef8d240224155e73ff11e8ac
Prosper <prosper.ekwerike@what3words.com>
2023-03-02 11:40:54 +0000

    updated script

--- circlecinative_build.sh
+++ circlecinative_build.sh
@@ -2,12 +2,9 @@
 set -e
 set -x
 
-export ANDROID_NDK_HOME="/Users/prosperekweike/Library/Android/sdk"
-export CMAKE_HOME=${ANDROID_NDK_HOME}/cmake/3.22.1/
-export CMAKE=${CMAKE_HOME}/bin/cmake
 export PLATFORM="21"
-export CMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/ndk/25.2.9519653/build/cmake/android.toolchain.cmake
-ACTUAL_NDK_HOME=${ANDROID_NDK_HOME}/ndk/25.2.9519653
+export CMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake
+
 
 # set the target - this will eventually be a for loop
 cd ./.circleci
@@ -23,10 +20,10 @@ for TARGET in "${TARGETS[@]}"; do
   cd build
 
   # cmake build
-  ${CMAKE}  ../../app/src/main/cpp \
+  cmake ../../app/src/main/cpp \
     -DBUILD_TESTS=NO \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
-    -DANDROID_NDK=${ACTUAL_NDK_HOME} \
+    -DANDROID_NDK=${ANDROID_NDK_HOME} \
     -DANDROID_ABI="${TARGET}" \
     -DANDROID_PLATFORM=${PLATFORM} \
     -DANDROID_TOOLCHAIN=clang \
